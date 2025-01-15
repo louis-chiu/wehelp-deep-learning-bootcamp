941fddffa2afc5a208b147e9f67cec4869d671cd
Louis Chiu <louis.yr.chiu@gmail.com>
2025-01-16 01:11:01 +0800

    week2: enhancement Point class
     - Point object's x and y values must be cast to string before passed into Decimal
     - distance_to method should not using math.hypot
     - distance_to method rounded to three decimal places

--- week-2/attachments.py
+++ week-2/attachments.py
@@ -1,15 +1,16 @@
 from typing import TypeVar, Self, Any
 
-from decimal import Decimal
-from math import hypot
+from decimal import Decimal, getcontext, ROUND_HALF_UP
+
+context = getcontext()
 
 Number = TypeVar("Number", int, float)
 
 
 class Point:
     def __init__(self, x: Number, y: Number) -> None:
-        self._x: Decimal = Decimal("{x}")
-        self._y: Decimal = Decimal("{y}")
+        self._x: Decimal = Decimal(f"{x}")
+        self._y: Decimal = Decimal(f"{y}")
 
     @property
     def x(self) -> Decimal:
@@ -17,7 +18,7 @@ class Point:
 
     @x.setter
     def x(self, x: Number):
-        self._x = Decimal("{x}")
+        self._x = Decimal(f"{x}")
 
     @property
     def y(self) -> Decimal:
@@ -25,7 +26,7 @@ class Point:
 
     @y.setter
     def y(self, y: Number):
-        self._y = Decimal("{y}")
+        self._y = Decimal(f"{y}")
 
     def delta_x(self, other: Self) -> Decimal:
         return self.x - other.x
@@ -40,12 +41,28 @@ class Point:
         return abs(self.delta_y(other))
 
     def distance_to(self, other: Self) -> Decimal:
+        """Calculate the distance from this point to another point.
+
+        Args:
+            other (Point): The other point to which the distance is calculated.
+
+        Returns:
+            Decimal: The distance between the two points, rounded to three decimal places.
+
+        Raises:
+            TypeError: If 'other' is not an instance of Point.
+        """
+
         if not isinstance(other, Point):
             raise TypeError(
                 f"Expected 'other' to be a Point, but got {type(other).__name__}."
             )
 
-        return Decimal(hypot(self.x_distance_to(other), self.y_distance_to(other)))
+        return (
+            (self.x_distance_to(other) ** 2 + self.y_distance_to(other) ** 2)
+            .sqrt(context)
+            .quantize(Decimal(".000"), ROUND_HALF_UP)
+        )
 
     def __eq__(self, other: Any) -> bool:
         if not isinstance(other, Point):
